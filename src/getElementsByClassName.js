a98dd75f545e1683512b936f1ab42df1fca49a26
dougecox <dougecox@gmail.com>
2017-03-20 00:43:15 -0700

    Add files via upload

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,5 +6,48 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className
 ) {
-  // your code here
+  // your code here below 3 noted to be used 
+  var bodyChildNodes = document.body.childNodes;
+  var bodyClassList = document.body.classList;
+  var docBody = document.body;
+  var results = [];
+
+  var findTarget = function( node ) {  //starting doc.body then> childNodes
+    if ( _.contains(node.classList, className ) ) {
+       results.push(node);
+     }
+    
+     _.forEach(node.childNodes, function(child) {
+      findTarget(child);
+    });
+   }
+
+   findTarget(docBody);
+   return results;
+ };
+
+/*
+
+    if ( obj.length === 1 && obj.contains(className) ) {
+       return obj;
+    }
+    else if (obj[0]['className'] === className) {  // use contains 
+      results.push(obj[0]);
+      return obj[0];
+    }
+    else if ( obj.length < 0 ) { // superfulsions
+      return null;
+    }
+    else {
+      return findTarget( Array.prototype.slice.call(obj, 1 ) );
+    }
+  };
+  
+  var sol = findTarget(document.body.childNodes) ;
+  console.log(results);
+  console.log( sol);
+  return  document.getElementsByClassName(className)
 };
+*/
+
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,73 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
-var stringifyJSON = function(obj) {
-  // your code goes here
+var stringifyJSON = function(input) {
+  // your code goes here GOAL convert keys, values to string
+  // account for nested 
+  var isPrimative = function(val) { return  ["string", "number"].includes(typeof val) };
+  var isObj = function(val) { return (typeof val === 'object' && !Array.isArray(val) ) };
+  var aAray = function(val) { return (typeof val === 'object' && Array.isArray(val) )};
+  // cases for null, undefined
+  //console.log('input');
+  //console.log(typeof input);
+  if (input === null) {
+    return 'null'; 
+  }
+  else if (typeof input === (undefined || 'undefined'  ) ) {
+    return ; 
+  }
+  else if (typeof input === 'function') {
+    return '{}';
+  }
+  // cases for primatives
+  else if ( typeof input === 'number' ){
+    return (  '' + input );
+  }
+  else if( typeof input === 'string') {
+    return '"' + input + '"';
+  }
+  else if( typeof input === 'boolean') {
+    return '' + input + '';
+  }
+  // cases for arrays
+  else if ( aAray(input) ) {
+    //check empty.arr
+    var partialArr = [];
+    if ( input.length === 0 ) {
+      return '[]';
+    }
+    else {
+      input.forEach( function(value) {
+       // console.log( typeof stringifyJSON(value) + " | " + value + " |s>| " + stringifyJSON(value) );
+        partialArr.push( stringifyJSON(value) );
+      });
+      return  '[' + partialArr.join() + ']';
+    }
+  }
+  //cases for object
+  else if ( isObj(input) ) {
+    var partialObj = [];
+    if (Object.keys(input)[0] === 'undefined' || typeof input === 'function') {
+      return '{}';
+    }
+    
+    for( var key in input) {
+      
+      if ( key === 'undefined' ) {
+       
+        return '{}'
+        break;
+      }
+      else {
+       //console.log("v: " +input[key]);
+       partialObj.push(stringifyJSON(key) + ':' + stringifyJSON(input[key]) );
+       //console.log("r: " + partialObj);
+      }
+    }
+    return "{" + partialObj.join() + "}";
+  }
+  
+  
 };
+
+
